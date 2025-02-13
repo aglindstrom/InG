390f19692e6d757e335ae88e2395cffb674f25f7
andrew lindstrom <lindstromandrew@gmail.com>
2025-02-13 15:36:37 -0500

    moved grouping from nodes to anchors. still needs some adjustments anchors should be excluded from their childrens grouping to prevent the great ball. Also re-styled the dropdown menus

--- GS_Frontend_NO_REACT/request.js
+++ GS_Frontend_NO_REACT/request.js
@@ -14,17 +14,15 @@ async function load_keys(){
                 .catch((error) => console.error(error));
 }
 
-// sources is an array of source objects 
-//  source object looks like this
-//  {port: ####, id: #}
-//  where # represents a numeric digit
-//
-// action_name is the display name of the undo action
+// Make a request for graph data using each node in a list of nodes as anchors
+// @params:
+//  nodes: the list of nodes to make request with
+//  action_name: name of the action in the undo stack
 
-function request_graph_data(sources, action_name){
+function request_graph_data(nodes, action_name){
     let requests = [];
-    sources.forEach((source) => {
-        requests.push(load_data(get_url(source), source.id));
+    nodes.forEach((node) => {
+        requests.push(load_data(get_url(node), node));
     });
 
     Promise.all(requests).then(() => {
@@ -35,9 +33,9 @@ function request_graph_data(sources, action_name){
 }
 
 async function request_neighbor_edges(node){
-    hide_menu(node_drop_down);
     let params = new URLSearchParams({codes: node.name, depth: 1, direction: node.direction});
     let url = `${invokeURL}:${node.source.port}/api/graph?${params.toString()}`;
+    
     return fetch(url).then((resp) => resp.json())
               .then((data) => update_group_links(data, node.name))
               .catch((error) => console.error(error));
@@ -45,7 +43,6 @@ async function request_neighbor_edges(node){
 
 function request_node_description(node){
     // use this to make node specific requests for extra node data from the extra node data server
-    hide_menu(drop_down);
     let output = document.getElementById("graph-console");
     let params = new URLSearchParams({node:node});
     fetch(`${invokeURL}:${description_source.port}/node/description?${params.toString()}`)
@@ -53,11 +50,16 @@ function request_node_description(node){
         .then((desc) => output.innerHTML = ">" + desc.description);
 }
 
-function get_url(source){
-    let request_selection = selected_nodes.filter((node) => source.id == node.group);
-    let directions = request_selection.map((node) => node.direction);
-    let selection = request_selection.map((node) => node.name);
-    let params = new URLSearchParams({codes: selection.join(","), depth: depth, directions: directions.join(",")});
-    let url = `${invokeURL}:${source.port}/api/graph?${params.toString()}`;
+
+// build a url to request a single node from the pre-defined server and port
+// @params:
+//  node: an Object that has a name, direction, and group
+
+function get_url(node){
+    let codes = node.name;
+    let directions = node.direction;
+    let params = new URLSearchParams({codes: codes, depth: depth, directions: directions});
+    let url = `${invokeURL}:${graph_sources[node.group].port}/api/graph?${params.toString()}`;
+    console.log(url);
     return url;
 }
