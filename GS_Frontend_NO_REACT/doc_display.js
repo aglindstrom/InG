2d5daab6f3d7eacbad2f297885cc9603c0a00e0d
Andrew <lindstromandrew@gmail.com>
2025-03-04 13:33:54 -0500

    add support functionality for entity selection in document

--- GS_Frontend_NO_REACT/doc_display.js
+++ GS_Frontend_NO_REACT/doc_display.js
@@ -20,8 +20,8 @@ function drop_handler(event){
         while(doc_display.firstChild){
             doc_display.removeChild(doc_display.firstChild);
         }
-        let doc_split = reader.result.split(/\s+/);
-        doc_split = doc_split.map((word) => {
+        const doc = reader.result;
+        const doc_split = doc.split(/\s+/).map((word) => {
             let span = document.createElement('span');
                 span.appendChild(document.createTextNode(word));
                 span.classList += "word";
@@ -39,13 +39,55 @@ function drop_handler(event){
             doc_display.appendChild(document.createElement('br'));
             doc_display.appendChild(node);
         });
+
+        const entities = extract_entities(doc);
+        highlight(doc_display, entities);
+        select_entity_nodes(entities);
     }
 
-    reader.readAsText(files[0]);    
+    reader.readAsText(files[0]);
 }
-
 
 
 function drag_over_handler(event){
     event.preventDefault();
+}
+
+function highlight(element, entities){
+    const children = [...element.children];
+    console.log(entities);
+    const words = children.filter((child) => 
+                  child.nodeName.toLowerCase() === 'span' && 
+                  child.className.includes('word'));
+
+    const isEntity = (word) => {
+        const idx = entities.indexOf(...entities.filter((e) => e.entity === word));
+        return idx;
+    }
+
+    words.forEach((word) => {
+        const idx = isEntity(word.textContent);
+        if(idx != -1){
+            const title = `entity node: ${entities[idx].node.name}`
+            word.classList.add('active');
+            word.title = title;
+        }
+    });
+}
+
+function extract_entities(data){
+    // TODO: Fetch from entity extractor 
+
+    //PLACEHOLDER//
+    const group = 1;
+    const direction = 1;
+    const data_s = data.split(/\s+/);
+    const data_f = data_s.filter(() => Math.random() > .75);
+    const data_out = data_f.map((d, idx) => ({entity: d, node:{name: idx, direction:direction, group:group}}));
+    return data_out;
+}
+
+function select_entity_nodes(entities){
+
+    entities.forEach((e) => select_node({name: e.node.name, direction: e.node.direction, group: e.node.group}));
 }
