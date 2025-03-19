c1a149d84924f74a59349736662221e55b96522a
andrew lindstrom <lindstromandrew@gmail.com>
2025-03-19 13:43:37 -0400

    Modified to work from npm script

--- GS_Backend/server.ps1
+++ GS_Backend/server.ps1
@@ -1,6 +1,6 @@
 param(
-  [string]$nodes = './data/ICD10CM,length=4.csv.gz',
-  [string]$edges = './data/DgPg-GenIndFwd,top_p=0.1.csv.gz',
+  [string]$nodes = './GS_Backend/data/ICD10CM,length=4.csv.gz',
+  [string]$edges = './GS_Backend/data/DgPg-GenIndFwd,top_p=0.1.csv.gz',
   [string]$names = 'dx10',
   [string]$port = '8000'
 )
